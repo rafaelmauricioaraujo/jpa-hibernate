a793c10424dbdb31159a3ffb9c0ba0e6ace72e8f
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-09-03 08:11:14 -0300

    creating tests class

---
+++ src/main/java/br/dev/learning/tests/CreateTableTests.java
@@ -0,0 +1,15 @@
+package br.dev.learning.tests;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class CreateTableTests {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Account");
+		EntityManager entityManager = emf.createEntityManager();
+		emf.close();
+	}
+
+}
