d6d349424f39d96ce34a5fc610c1b8ff9361b4fc
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-09-15 22:12:12 -0300

    lazy loading tests

---
+++ src/main/java/br/dev/learning/tests/TransactionReportTest.java
@@ -0,0 +1,26 @@
+package br.dev.learning.tests;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import br.dev.learning.model.Account;
+
+public class TransactionReportTest {
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Accounts");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		String jpql = "select acc from Account acc";
+		TypedQuery<Account> query = entityManager.createQuery(jpql, Account.class);
+		
+		List<Account> resultList = query.getResultList();
+		for (Account account : resultList) {
+			System.out.println("Owner: " + account.getOwner());
+			System.out.println("Transaction: " + account.getTransactions());
+		}
+	}
+}
